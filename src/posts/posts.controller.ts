c81af321c13cb5785a53a4d226c7978ce50e3e6f
Juan Canon <canonjp@javeriana.edu.co>
2025-08-23 20:02:14 -0500

    feat: controller routes for getting a single or all posts added

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Param, Post, Body, Get } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { Post as PostEntity} from './entities/post.entity';
@@ -9,10 +9,25 @@ import { Post as PostEntity} from './entities/post.entity';
 export class PostsController {
     constructor(private readonly postsService: PostsService) {}
 
+        // 1. CREATE post
         @Post()
         create(@Body() createPostDto: CreatePostDto): PostEntity {
             return this.postsService.create(createPostDto);
         }
 
+        // 2. RETRIEVE posts, either all or a single
+        @Get()
+        findAll(): PostEntity[]{
+            return this.postsService.findAll();
+        }
+
+        @Get(':id')
+        findOne(@Param('id') id: string): PostEntity{
+            return this.postsService.findOne(+id);
+        }
+
 }
+
+
+
 
