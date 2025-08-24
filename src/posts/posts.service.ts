3dad091e175fe74282122620046a53fda4a6fc9f
Juan Canon <canonjp@javeriana.edu.co>
2025-08-23 19:22:43 -0500

    feat: posts service for create post

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import {CreatePostDto} from './dto/create-post.dto'
+import {Post} from './entities/post.entity'
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+    //In-memory temporal storage while supabase isn't set
+    private posts: Post[] = []; 
+    //Auto-increment ID
+    private nextId = 1;
+
+    create(createPostDto: CreatePostDto): Post{
+        const newPost: Post = {
+            id: this.nextId++,
+            title: createPostDto.title,
+            content: createPostDto.content,
+            createdAt: new Date(),
+        };
+
+        this.posts.push(newPost);
+        return newPost;
+    }
+
+}
